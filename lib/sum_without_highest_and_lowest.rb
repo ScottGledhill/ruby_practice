859a8899511b1dc2459d31739c72f0d6a978db65
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-24 13:37:11 +0000

    tests passed

--- lib/sum_without_highest_and_lowest.rb
+++ lib/sum_without_highest_and_lowest.rb
@@ -5,6 +5,11 @@
 #  even if there are more than one with the same value!)
 class SumArray
   def sum(arr)
-    0
+    return 0 if arr == nil || arr == []
+    return 0 if arr.length < 2
+    sorted_array = arr.sort
+    new_array = sorted_array.take(arr.length - 1)
+    new_array.shift(1)
+    new_array.inject(0){|sum,x| sum + x }
   end
 end
