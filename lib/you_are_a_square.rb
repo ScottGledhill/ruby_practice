9bab7e2886f08cc31a1a5287b0c795130bc2352b
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-24 14:51:43 +0000

    test for not a perfect square

---
+++ lib/you_are_a_square.rb
@@ -0,0 +1,9 @@
+#
+# You like building blocks. You especially like building blocks that are squares.
+#  And what you even like more, is to arrange them into a square of square
+#   building blocks!
+#   However, sometimes, you can't arrange them into a square. Instead,
+#  you end up with an ordinary rectangle! Those blasted things!
+#   If you just had a way to know, whether you're currently working in vain…
+#   Wait! That's it! You just have to check if your number of building blocks
+#   is a perfect square.
---
+++ spec/you_are_a_square_spec.rb
@@ -0,0 +1,7 @@
+require 'you_are_a_square'
+
+describe Square do
+  it 'is not a perfect square' do
+    expect(subject.is_square(-1)).to eq false
+  end
+end
