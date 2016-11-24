3a6fc0e943a6d03f9bdad5e6f89cb583b903a952
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-24 16:47:57 +0000

    test for isogram false added

--- spec/isogram_spec.rb
+++ spec/isogram_spec.rb
@@ -5,4 +5,8 @@ describe Isogram do
     expect(subject.is_isogram?('Dermatoglyphics')).to eq true
     expect(subject.is_isogram?('isogram')).to eq true
   end
+
+  it 'has some repeating letters returns false' do
+    expect(subject.is_isogram?('aba')).to eq false
+  end
 end
