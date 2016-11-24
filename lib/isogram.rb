a68d82f6fc9ea5e21d74ccf6c828bed675219c4d
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-24 16:58:00 +0000

    added downcase method for mixed

--- lib/isogram.rb
+++ lib/isogram.rb
@@ -1,6 +1,6 @@
 class Isogram
   def is_isogram?(string)
-    split_string = string.scan(/[a-z]/i)
+    split_string = string.downcase.scan(/[a-z]/i)
     split_string.length == split_string.uniq.length
   end
 end
