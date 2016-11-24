a239d634efa3577b6408c9bd8baada9520aaa43a
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-24 14:52:31 +0000

    test for a perfect square

--- spec/you_are_a_square_spec.rb
+++ spec/you_are_a_square_spec.rb
@@ -2,6 +2,10 @@ require 'you_are_a_square'
 
 describe Square do
   it 'is not a perfect square' do
-    expect(subject.is_square(-1)).to eq false
+    expect(subject.is_square?(-1)).to eq false
+  end
+
+  it 'is  a perfect square' do
+    expect(subject.is_square?(4)).to eq true
   end
 end
