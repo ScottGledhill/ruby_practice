c533ea6c60a59a32abd6ec23a6afb52e3262321a
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-24 16:11:49 +0000

    square digits found

--- lib/square_every_digit.rb
+++ lib/square_every_digit.rb
@@ -1,4 +1,7 @@
 class SquareEveryDigit
   def square_digits(number)
+    array = number.to_s.split("")
+    numbered_array = array.map {|num| num.to_i }
+    numbered_array.map { |num| num * num}.join.to_i
   end
 end
--- spec/square_every_digit_spec.rb
+++ spec/square_every_digit_spec.rb
@@ -3,6 +3,6 @@ require 'square_every_digit'
 describe SquareEveryDigit do
   it 'digits can be squared' do
     expect(subject.square_digits(3212)).to eq 9414
-    expect(subject.square_digits(2112).to eq 4114
+    expect(subject.square_digits(2112)).to eq 4114
   end
 end
