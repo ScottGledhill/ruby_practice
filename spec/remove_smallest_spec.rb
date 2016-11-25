7c6223de29e2ce098e3d987d5562024bce5aa925
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-25 13:39:15 +0000

    test for empty array if only one element

--- spec/remove_smallest_spec.rb
+++ spec/remove_smallest_spec.rb
@@ -5,4 +5,7 @@ describe RemoveSmallest do
     expect(subject.remove_min([1, 2, 3, 4, 5])).to eq [2, 3, 4, 5]
   end
 
+  it 'returns empty array if only one element' do
+    expect(subject.remove_min([1])).to eq [1]
+  end
 end
