9cdcf4aea3d1e1fcb59cf09181aea0c56a4af1f5
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-28 20:56:57 +0000

    tests for both multiples of 5 and 3

--- spec/replace_multiples_with_strings_spec.rb
+++ spec/replace_multiples_with_strings_spec.rb
@@ -1,19 +1,16 @@
 require 'replace_multiples_with_strings'
 
 describe MultiplesWithStrings do
-  it 'team 2 wins' do
-    expect(subject.tug_of_war([[1,2,3,4,5],[2,2,3,4,5]])).to eq "Team 2 wins!"
+  it 'multiples of 3 is replaced with THREE' do
+    expect(subject.replace(3)).to eq "THREE"
+    expect(subject.replace(9)).to eq "THREE"
   end
 
-  it 'team 1 wins' do
-    expect(subject.tug_of_war([[9,2,3,4,5],[2,2,3,4,5]])).to eq "Team 1 wins!"
+  it 'multiples of 5 is replaced with FIVE' do
+    expect(subject.replace(5)).to eq "FIVE"
   end
 
-  it 'finds strongest team when tie using anchor' do
-    expect(subject.tug_of_war([[1,2,3,4,5],[1,2,3,4,5]])).to eq "Team 2 wins!"
-  end
-
-  it 'finds tie if both anchors and teams are equal' do
-    expect(subject.tug_of_war([[1,2,3,4,5],[5,4,3,2,1]])).to eq "It's a tie!"
+  it 'multiples of 5 and 3 replaced with 15' do
+    expect(subject.replace(15)).to eq "FIVE"
   end
 end
--- spec/tug_o_war_spec.rb
+++ spec/tug_o_war_spec.rb
@@ -1,10 +1,19 @@
 require 'tug_o_war'
 
 describe TugOfWar do
-  it '3 is replaced with THREE' do
-    expect(subject.replace(3).to eq "THREE"
+  it 'team 2 wins' do
+    expect(subject.tug_of_war([[1,2,3,4,5],[2,2,3,4,5]])).to eq "Team 2 wins!"
   end
 
-  it '5 is replaced with FIVE' do
-    expect(subject.replace(3).to eq "FIVE"
+  it 'team 1 wins' do
+    expect(subject.tug_of_war([[9,2,3,4,5],[2,2,3,4,5]])).to eq "Team 1 wins!"
+  end
+
+  it 'finds strongest team when tie using anchor' do
+    expect(subject.tug_of_war([[1,2,3,4,5],[1,2,3,4,5]])).to eq "Team 2 wins!"
+  end
+
+  it 'finds tie if both anchors and teams are equal' do
+    expect(subject.tug_of_war([[1,2,3,4,5],[5,4,3,2,1]])).to eq "It's a tie!"
+  end
 end
