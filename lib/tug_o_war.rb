eb49a720d209a54dc88a3de3b44a328ee95d75cd
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-28 20:48:53 +0000

    tug of war outcome completed

--- lib/tug_o_war.rb
+++ lib/tug_o_war.rb
@@ -4,6 +4,10 @@ class TugOfWar
     team2 = teams.last(1)
     team2[0].reverse!
     return "It's a tie!" if team1 == team2
-    team1[0].inject(:+) > team2[0].inject(:+) ? "Team 1 wins!" : "Team 2 wins!"
+    if team1[0].inject(:+) == team2[0].inject(:+)
+      team1[0][-1] > team2[0][-1] ? "Team 2 wins!" : "Team 1 wins!"
+    else
+      team1[0].inject(:+) > team2[0].inject(:+) ? "Team 1 wins!" : "Team 2 wins!"
+    end
   end
 end
