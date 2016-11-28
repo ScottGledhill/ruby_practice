5e9110589084753c8caf2656aad7e35c9823ef20
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-28 19:11:36 +0000

    mangos completed

--- lib/mangoes.rb
+++ lib/mangoes.rb
@@ -1,2 +1,8 @@
 class Mangoes
   def mango(quantity,price)
+    return quantity * price if quantity < 3
+    return (quantity / 3) * 2 * price if quantity % 3 == 0
+    return (quantity / 3) * 2 * price + price * 2 if quantity % 3 == 2
+    return (quantity / 3) * 2 * price + price * 1
+  end
+end
--- spec/mangoes_spec.rb
+++ spec/mangoes_spec.rb
@@ -2,10 +2,11 @@ require 'mangoes'
 
 describe Mangoes do
   it 'find price if less than 3 bought' do
-    expect(subject.mango(3,3)).to eq 6
+    expect(subject.mango(0,5)).to eq 0
   end
 
   it 'find price if 3 or more' do
-    expect(subject.mango(0,5)).to eq 0
-    expect(subject.mango(9,0)).to eq 0
+    expect(subject.mango(3,3)).to eq 6
+    expect(subject.mango(9,5)).to eq 30
+  end
 end
