982848b53a81124d4d808a32aa76dc4fb91d37b8
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-28 18:48:54 +0000

    test for 3 or more added

--- spec/mangoes_spec.rb
+++ spec/mangoes_spec.rb
@@ -4,4 +4,8 @@ describe Mangoes do
   it 'find price if less than 3 bought' do
     expect(subject.alternate_case(3,3)).to eq 6
   end
+
+  it 'find price if 3 or more' do
+    expect(subject.alternate_case(0,5)).to eq 0
+    expect(subject.alternate_case(9,0)).to eq 0
 end
