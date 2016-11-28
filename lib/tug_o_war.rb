91466b865942de1d79d711ece21a2d00a3bc1a64
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-28 20:32:01 +0000

    test added for team 1 wins

--- lib/tug_o_war.rb
+++ lib/tug_o_war.rb
@@ -1,7 +1,7 @@
 class TugOfWar
   def tug_of_war(teams)
-    p team1 = teams.first(1)
-    p team2 = teams.last(1)
-    
+    team1 = teams.first(1)
+    team2 = teams.last(1)
+    team1[0].inject(:+) > team2[0].inject(:+) ? "Team 1 wins!" : "Team 2 wins!"
   end
 end
--- spec/tug_o_war_spec.rb
+++ spec/tug_o_war_spec.rb
@@ -1,8 +1,12 @@
 require 'tug_o_war'
 
 describe TugOfWar do
-  it 'finds strongest team' do
+  it 'team 2 wins' do
     expect(subject.tug_of_war([[1,2,3,4,5],[2,2,3,4,5]])).to eq "Team 2 wins!"
+  end
+
+  it 'team 1 wins' do
+    expect(subject.tug_of_war([[9,2,3,4,5],[2,2,3,4,5]])).to eq "Team 1 wins!"
   end
 
   it 'finds strongest team when tie using anchor' do
