60f8c8b6f889c099e35065dda46a8f4b9e3d1218
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-28 20:55:05 +0000

    five replaced with five

--- spec/tug_o_war_spec.rb
+++ spec/tug_o_war_spec.rb
@@ -4,3 +4,7 @@ describe TugOfWar do
   it '3 is replaced with THREE' do
     expect(subject.replace(3).to eq "THREE"
   end
+
+  it '5 is replaced with FIVE' do
+    expect(subject.replace(3).to eq "FIVE"
+end
