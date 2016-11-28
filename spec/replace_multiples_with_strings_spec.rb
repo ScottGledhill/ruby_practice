39584ed6be943e44249e587ba6c0f9533ca7e3e8
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-28 21:01:51 +0000

    logic for getnumber done

--- spec/replace_multiples_with_strings_spec.rb
+++ spec/replace_multiples_with_strings_spec.rb
@@ -1,16 +1,16 @@
 require 'replace_multiples_with_strings'
 
 describe MultiplesWithStrings do
-  it 'multiples of 3 is replaced with THREE' do
-    expect(subject.replace(3)).to eq "THREE"
-    expect(subject.replace(9)).to eq "THREE"
+  it 'multiples of 3 are replaced with THREE' do
+    expect(subject.getNumber(3)).to eq "THREE"
+    expect(subject.getNumber(9)).to eq "THREE"
   end
 
-  it 'multiples of 5 is replaced with FIVE' do
-    expect(subject.replace(5)).to eq "FIVE"
+  it 'multiples of 5 are replaced with FIVE' do
+    expect(subject.getNumber(5)).to eq "FIVE"
   end
 
   it 'multiples of 5 and 3 replaced with 15' do
-    expect(subject.replace(15)).to eq "FIVE"
+    expect(subject.getNumber(15)).to eq "BOTH"
   end
 end
