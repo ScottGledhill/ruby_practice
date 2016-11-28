037abb3b29962576aaddee3c5255f49fe3839edb
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-28 21:05:49 +0000

    test for multiples of neither

--- lib/replace_multiples_with_strings.rb
+++ lib/replace_multiples_with_strings.rb
@@ -3,6 +3,7 @@ class MultiplesWithStrings
     return "BOTH" if number % 5 == 0 && number % 3 == 0
     return "THREE" if number % 3 == 0
     return "FIVE" if number % 5 == 0
+    number
   end
 
   def getNumberRange(first, last)
--- spec/replace_multiples_with_strings_spec.rb
+++ spec/replace_multiples_with_strings_spec.rb
@@ -13,4 +13,8 @@ describe MultiplesWithStrings do
   it 'multiples of 5 and 3 replaced with 15' do
     expect(subject.getNumber(15)).to eq "BOTH"
   end
+
+  it 'returns number if multiple of neither 5 or 3' do
+    expect(subject.getNumber(17)).to eq 17
+  end
 end
