a33dc09a88cdb52c187b69b628d0a973739ecf68
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-28 20:18:35 +0000

    test for tie

--- spec/tug_o_war_spec.rb
+++ spec/tug_o_war_spec.rb
@@ -8,4 +8,8 @@ describe TugOfWar do
   it 'finds strongest team when tie using anchor' do
     expect(subject.tug_of_war([[1,2,3,4,5],[1,2,3,4,5]])).to eq "Team 2 wins!, Team 2 has a stronger anchor."
   end
+
+  it 'finds tie if both anchors and teams are equal' do
+    expect(subject.tug_of_war([[1,2,3,4,5],[5,4,3,2,1]])).to eq "It's a tie!", "Equal team and Anchor strengths."
+  end
 end
