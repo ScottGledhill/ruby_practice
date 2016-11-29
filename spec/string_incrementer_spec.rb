9f7a9ecce4fff9b37b43cf466b72f2d9f18ed063
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-29 17:57:45 +0000

    test for leading zeros

--- spec/string_incrementer_spec.rb
+++ spec/string_incrementer_spec.rb
@@ -5,4 +5,7 @@ describe StringIncrementer do
     expect(subject.increment_string('foo')).to eq 'foo1'
   end
 
+  it 'adds 1 onto string with leading zeros' do
+    expect(subject.increment_string('foobar00')).to eq 'foobar01'
+  end
 end
