08c48b31ec883fff66d5824e6af4a3fd98ce550a
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-29 18:16:06 +0000

    string incrementer

--- lib/string_incrementer.rb
+++ lib/string_incrementer.rb
@@ -1,4 +1,6 @@
 class StringIncrementer
-  def increment_string
-  end
+  def increment_string(input)
+    num_str = input.scan(/\d+$/).first.to_s
+    return input.gsub(num_str, num_str.next) if input.match(/\d$/)
+    return input + "1" if num_str.empty?  end
 end
