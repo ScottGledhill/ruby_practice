bf64da78fb52193fe19d39681b175c502ece5a82
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-29 13:24:32 +0000

    test for pass

--- spec/to_buy_or_not_spec.rb
+++ spec/to_buy_or_not_spec.rb
@@ -5,4 +5,7 @@ describe ToBuyOrNot do
     expect(subject.buy_or_pass(46.74, 58.43)).to eq "Buy"
   end
 
+  it 'returns number multiplied by 50 and increased by 6' do
+    expect(subject.buy_or_pass(55,56)).to eq "Pass"
+  end
 end
