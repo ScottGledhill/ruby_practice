e5404fbe09f334958123ef9f4aeef960e1cf2517
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-29 13:13:44 +0000

    test for multipled by 50 add 6"

--- spec/super_duper_easy_spec.rb
+++ spec/super_duper_easy_spec.rb
@@ -1,7 +1,11 @@
 require 'super_duper_easy'
 
-describe AlternatingCase do
+describe SuperDuperEasy do
   it 'returns error when string given' do
-    expect(subject.alternate_case("hello world")).to eq "Error"
+    expect(subject.problem("hello world")).to eq "Error"
+  end
+
+  it 'returns number multiplied by 50 and increased by 6' do
+    expect(subject.problem(3).to eq 156
   end
 end
