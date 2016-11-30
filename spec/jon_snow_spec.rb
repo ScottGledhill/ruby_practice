acf18ac73623d4b952fa2b2a97fd921018088297
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-30 17:23:25 +0000

    test for if jon snows parents

--- spec/jon_snow_spec.rb
+++ spec/jon_snow_spec.rb
@@ -4,4 +4,8 @@ describe JonSnow do
   it 'false if not jon snows parents' do
     expect(subject.find_parents("sense Does", "to That Make you?")).to eq "You know nothing, Jon Snow"
   end
+
+  it 'true if jon snows parents' do
+    expect(subject.find_parents("Rhaegar Targaryen", "Lyanna Stark")).to eq "Jon Snow you deserve the throne"
+  end
 end
