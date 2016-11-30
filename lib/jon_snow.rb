e18e23aa1da6c3b46cbdc361e66f2af313787d3c
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-30 17:24:19 +0000

    omg found his parents

---
+++ lib/jon_snow.rb
@@ -0,0 +1,6 @@
+class JonSnow
+  def find_parents(dad,mom)
+    return "Jon Snow you deserve the throne" if dad == "Rhaegar Targaryen" && mom == "Lyanna Stark"
+    "You know nothing, Jon Snow"
+  end
+end
