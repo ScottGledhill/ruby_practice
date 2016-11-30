a3ccb016cca092f2efa2db47ab70cfff09225a9b
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-30 18:09:30 +0000

    pluralize test for es

--- spec/pluralize_spec.rb
+++ spec/pluralize_spec.rb
@@ -4,4 +4,8 @@ describe Pluralize do
   it 'adds s' do
     expect(subject.pluralize('table')).to eq 'tables'
   end
+
+  it 'adds es' do
+    expect(subject.pluralize('watch')).to eq 'watches'
+  end
 end
