0163c24340765d9e6475a174d7c4d86f9a84c1b1
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-30 17:28:29 +0000

    test for strings

--- spec/sum_of_all_arguments_spec.rb
+++ spec/sum_of_all_arguments_spec.rb
@@ -8,4 +8,8 @@ describe Sum do
   it 'can sum negative numbers also' do
     expect(subject.sum_all(76856,-32,1981,1076)).to eq 79881
   end
+
+  it 'test for strings' do
+    expect(subject.sum_all(1,-32,"codewars",1076)).to eq false
+  end
 end
