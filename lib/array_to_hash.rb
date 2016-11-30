c87fd611d435c77b46f7f261390b74aad20966b4
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-30 18:54:16 +0000

    hash converted into array

--- lib/array_to_hash.rb
+++ lib/array_to_hash.rb
@@ -1,4 +1,7 @@
 class ArrayToHash
   def to_hash(array)
+    Hash[array]
   end
 end
+
+# array.inject(Hash.new(0)) { |total, e| total[e] += 1 ;total}
--- spec/array_to_hash_spec.rb
+++ spec/array_to_hash_spec.rb
@@ -4,5 +4,4 @@ describe ArrayToHash do
   it 'can walk home' do
     expect(subject.to_hash([["cat", "dog"], ["duck", "donkey"]])).to eq "cat" => "dog", "duck" => "donkey"
   end
-
 end
