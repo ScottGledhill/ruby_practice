98a2ec661f32dbc8d54595214972f8b9fc0212f1
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-30 17:27:48 +0000

    negative numbers sum test

--- spec/sum_of_all_arguments_spec.rb
+++ spec/sum_of_all_arguments_spec.rb
@@ -5,4 +5,7 @@ describe Sum do
     expect(subject.sum_all(6,2,3)).to eq 11
   end
 
+  it 'can sum negative numbers also' do
+    expect(subject.sum_all(76856,-32,1981,1076)).to eq 79881
+  end
 end
