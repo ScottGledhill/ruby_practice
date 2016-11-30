c4d012da0f58365ec1b071ddc9e0af4abe75ab33
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-11-30 18:41:09 +0000

    refactored robots

--- lib/robot_walk.rb
+++ lib/robot_walk.rb
@@ -1,15 +1,5 @@
 class RobotWalk
   def walk_home?(directions)
-    array = directions.split('')
-    hash = array.inject(Hash.new(0)) { |total, e| total[e] += 1 ;total}
-    north = hash['n']
-    south = hash['s']
-    east = hash['e']
-    west = hash['w']
-    return true if north == south && east == west
-    false
+    directions.count('n') == directions.count('s') && directions.count('e') == directions.count('w')
   end
 end
-
-
-# str.count('n') == str.count('s') && str.count('e') == str.count('w')
