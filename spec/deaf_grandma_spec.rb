48276403448edf5e80842110405186b720086115
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-01 17:38:52 +0000

    test for caps deaf grandma

--- spec/deaf_grandma_spec.rb
+++ spec/deaf_grandma_spec.rb
@@ -4,4 +4,8 @@ describe DeafGrandma do
   it 'speak up if not in capitals' do
     expect(subject.deaf('hllo')).to eq 'HUH?! SPEAK UP, SONNY!'
   end
+
+  it 'if you type in caps she shouts not since 1983' do
+    expect(subject.deaf('HELLO')).to eq 'NO, NOT SINCE 1938!'
+  end
 end
