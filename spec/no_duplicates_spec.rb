2a7bb06ea2b5e2d89da1b2dba5710a78f3d6ffdb
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-01 12:59:45 +0000

    test for not an array

--- spec/no_duplicates_spec.rb
+++ spec/no_duplicates_spec.rb
@@ -2,11 +2,16 @@ require 'no_duplicates'
 
 describe NoDuplicates do
 
+  it 'removes duplicates' do
+    expect(subject.remove([1, 2, 3, 4, 1, 2, 3, 4])).to eq [1, 2, 3, 4]
+  end
+
   it 'removes duplicates and orders numbers first' do
     expect(subject.remove(["g", 3, "a", "a"])).to eq [ 3, 'a', 'g' ]
   end
 
-  it 'removes duplicates' do
-    expect(subject.remove([1, 2, 3, 4, 1, 2, 3, 4])).to eq [1, 2, 3, 4]
+  it 'returns Not an array if arg not an array' do
+    expect(subject.remove("")).to eq "Not an array"
+
   end
 end
