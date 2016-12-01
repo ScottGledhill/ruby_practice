5828c147bcdab680e7e51656e64fe2e16913293a
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-01 13:51:40 +0000

    test for hello world

---
+++ lib/hello_name.rb
@@ -0,0 +1,5 @@
+class HelloName
+  def hello
+
+  end
+end
--- spec/hello_name_spec.rb
+++ spec/hello_name_spec.rb
@@ -3,6 +3,10 @@ require 'hello_name'
 describe HelloName do
 
   it 'puts hello name with capital' do
-    expect(subject.hello('johN').to eq "Hello, John!"
+    expect(subject.hello('johN')).to eq "Hello, John!"
+  end
+
+  it 'puts hello world if no name given' do
+    expect(subject.hello()).to eq "Hello, World!"
   end
 end
