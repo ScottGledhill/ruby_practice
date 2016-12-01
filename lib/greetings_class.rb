bd6bd53adbe959516c690203db4e2744dced8be2
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-01 12:50:57 +0000

    class setup initialize method added

---
+++ lib/greetings_class.rb
@@ -0,0 +1,10 @@
+class Person
+  attr_reader :first, :last
+  def initialize(first,last)
+    @first = first
+    @last = last
+  end
+
+  def greet
+  end
+end
--- spec/greetings_class_spec.rb
+++ spec/greetings_class_spec.rb
@@ -1,17 +1,19 @@
 require 'greetings_class'
 
 describe Person do
-  Person.new('bob', 'bobbly')
 
   it 'initializes with first name' do
-    expect(subject.first).to eq 'bob'
+    person = Person.new('bob', 'bobbly')
+    expect(person.first).to eq 'bob'
   end
 
   it 'initializes with last name' do
-    expect(subject.last).to eq 'bobbly'
+    person = Person.new('bob', 'bobbly')
+    expect(person.last).to eq 'bobbly'
   end
 
   it 'greets with first and last name' do
-    expect(subject.greet).to eq 'Hello, Bob Smith!'
+    person = Person.new('bob', 'bobbly')
+    expect(person.greet).to eq 'Hello, Bob Smith!'
   end
 end
