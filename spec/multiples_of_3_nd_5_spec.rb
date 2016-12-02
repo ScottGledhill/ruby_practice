3e45dab9b4ce13853b526b22bb6874d66ed40505
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-02 18:59:23 +0000

    test for multiples of 3 and 5

---
+++ spec/multiples_of_3_nd_5_spec.rb
@@ -0,0 +1,7 @@
+require 'multiples_of_3_and_5'
+
+describe Multiples do
+  it 'sum of all numbers below number if multiple of 3 or 5' do
+    expect(subject.solution('hllo')).to eq 'HUH?! SPEAK UP, SONNY!'
+  end
+end
