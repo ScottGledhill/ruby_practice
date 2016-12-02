b2d4d999fa67130fdc08b491a8972c0305b9652e
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-02 13:47:28 +0000

    touchdown test for false

--- spec/touchdown_spec.rb
+++ spec/touchdown_spec.rb
@@ -4,4 +4,8 @@ describe Touchdown do
   it 'returns true if touchdown scored' do
     expect(subject.touchdown?([90, 5.4, 4.6])).to eq true
   end
+
+  it 'returns true if touchdown scored' do
+    expect(subject.touchdown?([20, 1, 2, 3, 3, 5, 66])).to eq false
+  end
 end
