3ed68ffd8982837d48b490903037882106fa6beb
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-02 15:12:55 +0000

    difference of squares

--- lib/difference_of_squares.rb
+++ lib/difference_of_squares.rb
@@ -1,1 +1,7 @@
-
+class DifferenceOfSquares
+  def difference_of_squares(x)
+    number = (1..x).map {|num| num ** 2}.inject(:+)
+    num2 = (1..x).inject(:+) ** 2
+    num2 - number
+  end
+end
--- spec/difference_of_squares_spec.rb
+++ spec/difference_of_squares_spec.rb
@@ -2,6 +2,6 @@ require 'difference_of_squares'
 
 describe DifferenceOfSquares do
   it 'difference between the sum of the squares' do
-    expect(subject.convert(1).to eq 0
+    expect(subject.difference_of_squares(1)).to eq 0
   end
 end
