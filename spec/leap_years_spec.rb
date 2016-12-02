2bf213949a1d94ab483a4d7d137eb0b08140c60c
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-02 14:36:36 +0000

    test for century exceptions

--- spec/leap_years_spec.rb
+++ spec/leap_years_spec.rb
@@ -8,4 +8,7 @@ describe LeapYears do
   it 'returns 366 if not a leap year' do
     expect(subject.year_days(2016)).to eq '2016 has 366 days'
   end
+
+  it 'tests for century exceptions' do
+    expect(subject.year_days(-300)).to eq '-300 has 365 days'
 end
