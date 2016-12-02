b0e48b1942ee836d895209f612a8288a9ab745d0
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-02 14:35:11 +0000

    test for not a leap year

--- spec/leap_years_spec.rb
+++ spec/leap_years_spec.rb
@@ -4,4 +4,8 @@ describe LeapYears do
   it 'returns 365 if a leap year' do
     expect(subject.year_days(-10)).to eq '-10 has 365 days'
   end
+
+  it 'returns 366 if not a leap year' do
+    expect(subject.year_days(2016)).to eq '2016 has 366 days'
+  end
 end
