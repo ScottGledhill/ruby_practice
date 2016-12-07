3ecbf4374b72ff1665bfdf8e75be718e69ec461b
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-07 13:54:03 +0000

    two lowest int found

---
+++ lib/sum_lowest_int.rb
@@ -0,0 +1,6 @@
+class Sum
+  def total(numbers)
+    numbers = numbers.sort
+    numbers[0] + numbers[1]
+  end
+end
--- spec/sum_lowest_int_spec.rb
+++ spec/sum_lowest_int_spec.rb
@@ -1,6 +1,6 @@
 require 'sum_lowest_int'
 
-describe WhoLikes do
+describe Sum do
   it 'sums the two lowest integers in an array' do
     expect(subject.total([5, 8, 12, 18, 22])).to eq 13
   end
