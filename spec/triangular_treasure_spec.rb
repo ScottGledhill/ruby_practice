771cf8c503c367c4d7b519b566826c92db586495
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-07 14:21:38 +0000

    test for triangulating treasure

--- spec/triangular_treasure_spec.rb
+++ spec/triangular_treasure_spec.rb
@@ -1,7 +1,11 @@
 require 'triangular_treasure'
 
 describe Triangular do
-  it 'sums the two lowest integers in an array' do
+  it 'adds up the total of 1..n' do
     expect(subject.treasure(0).to eq 0
+  end
+
+  it 'sums the two lowest integers in an array' do
+    expect(subject.treasure(3).to eq 6
   end
 end
