d99eb8dea701f539ba7ecb0c15d2a35bbb3def6d
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-07 12:39:45 +0000

    test for 3 people liking it

--- spec/who_likes_this_spec.rb
+++ spec/who_likes_this_spec.rb
@@ -9,6 +9,9 @@ describe WhoLikes do
     expect(subject.likes(['Peter'])).to eq 'Peter likes this'
   end
 
+  it 'returns 3 names if 3 people like it' do
+    expect(subject.likes(['Max', 'John', 'Mark']).to eq 'Max, John and Mark like this'
+  end
   it 'returns who likes it if multiple people' do
     expect(subject.likes(['Alex', 'Jacob', 'Mark', 'Max']).to eq 'Alex, Jacob and 2 others like this'
   end
