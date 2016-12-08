06f2c99a3e0edb2e0350b99146c1b2caf7555589
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-08 14:14:49 +0000

    returns false if prime number

--- spec/longest_spec.rb
+++ spec/longest_spec.rb
@@ -4,4 +4,8 @@ describe Prime do
   it 'returns true if prime' do
     expect(subject.is_prime?(2)).to eq true
   end
+
+  it 'returns false if prime' do
+    expect(subject.is_prime?(6)).to eq false
+  end
 end
