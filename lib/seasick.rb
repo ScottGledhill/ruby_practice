cacbd312258f9df679c292fea51271ebca27dcf2
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-08 13:06:20 +0000

    seasick solved

--- lib/seasick.rb
+++ lib/seasick.rb
@@ -1,5 +1,8 @@
 class Seasick
   def seasick(waves)
-    
+    wave_to_calm = (waves.scan(/(?=_~)/)).length
+    calm_to_wave = (waves.scan(/(?=~_)/)).length
+    changes = wave_to_calm + calm_to_wave
+    (changes > waves.length/5) ? "Throw Up" : "No Problem"
   end
 end
