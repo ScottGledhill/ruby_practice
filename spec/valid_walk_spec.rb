e9b942db0dad48b7f156c1c1da522bea0f315516
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-08 13:13:32 +0000

    valid walk returns false is more than ten min walk

--- spec/valid_walk_spec.rb
+++ spec/valid_walk_spec.rb
@@ -4,4 +4,8 @@ describe ValidWalk do
   it 'returns false if length is less than 10' do
     expect(subject.walk['w']).to eq 'should return false'
   end
+
+  it 'returns false if length is longer than 10' do
+    expect(subject.walk['n','n','n','s','n','s','n','s','n','s']).to eq 'should return false'
+  end
 end
