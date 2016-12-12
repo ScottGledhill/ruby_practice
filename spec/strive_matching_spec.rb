1233f0209c9803238812bb2dba70b0ef34d77b5d
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-12 17:03:34 +0000

    test for if jpb not met

--- spec/strive_matching_spec.rb
+++ spec/strive_matching_spec.rb
@@ -10,10 +10,14 @@ describe StriveMatching do
   it 'returns false if salary min, not within 10% of job min' do
     candidate2 = { 'min_salary'=>1200000 }
     job2 = { 'max_salary'=>130000 }
-    expect(subject.match(candidate1, job1)).to eq false
+    expect(subject.match(candidate2, job2)).to eq false
   end
 
   it 'raises error when no candidate' do
-    expect(subject.match(candidate1, job1)).to raise_error
+    expect(subject.match({}, job1)).to raise_error
+  end
+
+  it 'raises error when no job' do
+    expect(subject.match(candidate1,{})).to raise_error
   end
 end
