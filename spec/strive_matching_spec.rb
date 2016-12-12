8fbb6f9aae864178e9dc9dfbe3e1eff84f75f5a8
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-12-12 17:01:31 +0000

    test for if salary not met

--- spec/strive_matching_spec.rb
+++ spec/strive_matching_spec.rb
@@ -6,4 +6,9 @@ describe StriveMatching do
     job1 = { 'max_salary'=>130000 }
     expect(subject.match(candidate1, job1)).to eq true
   end
+
+  it 'returns false if salary min, not within 10% of job min' do
+    candidate2 = { 'min_salary'=>1200000 }
+    job2 = { 'max_salary'=>130000 }
+    expect(subject.match(candidate1, job1)).to eq false
 end
